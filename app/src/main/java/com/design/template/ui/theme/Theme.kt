42606dfdcfedcddc451f911e54e2143d452d29ed
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-20 13:07:55 +0100

    Update Theme.kt

--- app/src/main/java/com/design/template/ui/theme/Theme.kt
+++ app/src/main/java/com/design/template/ui/theme/Theme.kt
@@ -1,10 +1,11 @@
-package com.yourapp.ui.theme
+package com.design.template.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
     primary = LightPrimary,
