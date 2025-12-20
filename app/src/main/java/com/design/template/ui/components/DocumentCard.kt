bcd7f354d50d935b42614267ddb6fff470ba11b8
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-20 15:15:33 +0100

    Update DocumentCard.kt

--- app/src/main/java/com/design/template/ui/components/DocumentCard.kt
+++ app/src/main/java/com/design/template/ui/components/DocumentCard.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import com.design.template.ui.theme.Dimens
 import com.design.template.ui.theme.customColors
 
