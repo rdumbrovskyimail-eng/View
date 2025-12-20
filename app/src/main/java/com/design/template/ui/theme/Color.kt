b074f20065159034ceb88a16d1c1c066c635fe16
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-20 12:45:07 +0100

    Update Color.kt

--- app/src/main/java/com/design/template/ui/theme/Color.kt
+++ app/src/main/java/com/design/template/ui/theme/Color.kt
@@ -1,33 +1,33 @@
-package com.design.template.ui.theme
+package com.yourapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Light (адаптировано под Grok: минималистичные, светлые тона с акцентом на синий/серый)
-val Primary = Color(0xFF64748B)          // Спокойный серо-синий как в Grok
-val OnPrimary = Color.White
-val PrimaryContainer = Color(0xFFE2E8F0)
+// Light Theme Colors
+val LightSurface = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFF8F8F8)
+val LightPrimary = Color(0xFFB84A2E)
+val LightSecondary = Color(0xFF1565C0)
+val LightText = Color(0xFF1A1A1A)
+val LightTextSecondary = Color(0xFF616161)
+val LightOutline = Color(0xFFD0D0D0)
+val LightCardBg = Color(0xFFFAFAFA)
+val LightChipBg = Color(0xFFE8E8E8)
+val LightFieldBg = Color(0xFFF5F5F5)
+val LightDivider = Color(0xFFDBDBDB)
+val LightError = Color(0xFFD32F2F)
+val LightErrorBg = Color(0xFFFFEBEE)
 
-val Secondary = Color(0xFF2563EB)
-val SecondaryContainer = Color(0xFFDDE7FF)
-
-val Background = Color(0xFFF8FAFC)       // Светлый фон Grok
-val Surface = Color(0xFFFFFFFF)
-val SurfaceVariant = Color(0xFFF1F5F9)
-val SurfaceContainer = Color(0xFFF8FAFC)
-
-val OnSurface = Color(0xFF0F172A)
-val OnSurfaceVariant = Color(0xFF475569)
-
-val Outline = Color(0xFFCBD5E1)
-val OutlineVariant = Color(0xFFE2E8F0)
-
-val Error = Color(0xFFDC2626)
-
-// Dark (для опциональной тёмной темы, но фокус на светлой как в Grok)
-val DarkPrimary = Color(0xFF93C5FD)
-val DarkBackground = Color(0xFF020617)
-val DarkSurface = Color(0xFF020617)
-val DarkSurfaceVariant = Color(0xFF020617)
-val DarkOnSurface = Color(0xFFE5E7EB)
-val DarkOnSurfaceVariant = Color(0xFF9CA3AF)
-val DarkOutline = Color(0xFF1F2937)
+// Dark Theme Colors
+val DarkSurface = Color(0xFF1A1A1A)
+val DarkBackground = Color(0xFF121212)
+val DarkPrimary = Color(0xFFD4735E)
+val DarkSecondary = Color(0xFF4A8FDB)
+val DarkText = Color(0xFFE5E5E5)
+val DarkTextSecondary = Color(0xFF9E9E9E)
+val DarkOutline = Color(0xFF2F2F2F)
+val DarkCardBg = Color(0xFF242424)
+val DarkChipBg = Color(0xFF2A2A2A)
+val DarkFieldBg = Color(0xFF1E1E1E)
+val DarkDivider = Color(0xFF333333)
+val DarkError = Color(0xFFEF5350)
+val DarkErrorBg = Color(0xFF3D1F1F)
