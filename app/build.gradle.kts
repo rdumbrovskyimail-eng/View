8199acad68b7eddcab52805563d21f7f3a21a517
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-21 03:31:04 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
