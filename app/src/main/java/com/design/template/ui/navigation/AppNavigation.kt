a9f56e43bb3fc33a91f7926d2589d88c95121eb2
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-21 03:09:09 +0100

    Update AppNavigation.kt

--- app/src/main/java/com/design/template/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/design/template/ui/navigation/AppNavigation.kt
@@ -1,26 +1,45 @@
 package com.design.template.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.design.template.DocumentScannerApp
+import com.design.template.ui.screens.DocumentEditorScreen
+import com.design.template.ui.screens.DocumentItem
 
 @Composable
-fun AppNavigation(
-    navController: NavHostController = rememberNavController()
-) {
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    // временные данные, чтобы приложение СТАРТОВАЛО
+    val demoDocuments = listOf(
+        DocumentItem(
+            id = "1",
+            scannedText = "Scanned text example",
+            translatedText = "Translated text example"
+        )
+    )
+
     NavHost(
         navController = navController,
-        startDestination = AppRoute.Main.route
+        startDestination = "editor"
     ) {
-        composable(route = AppRoute.Main.route) {
-            DocumentScannerApp()
+        composable("editor") {
+            DocumentEditorScreen(
+                folderName = "My Documents",
+                documentTitle = "Document title",
+                documentDescription = "Document description",
+                documents = demoDocuments,
+                onBackClick = {},
+                onMenuClick = {},
+                onCameraClick = {},
+                onGalleryClick = {},
+                onGptClick = {},
+                onCopyClick = {},
+                onPasteClick = {},
+                onShareClick = {},
+                onDeleteClick = {}
+            )
         }
     }
-}
-
-sealed class AppRoute(val route: String) {
-    data object Main : AppRoute("main")
 }
