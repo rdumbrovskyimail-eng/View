3e8a3a19fc437b12033025b0592f5c9b8ae2372c
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-21 03:28:35 +0100

    Update App.kt

--- app/src/main/java/com/design/template/App.kt
+++ app/src/main/java/com/design/template/App.kt
@@ -3,7 +3,26 @@ package com.design.template
 import android.app.Application
 
 class App : Application() {
+    
+    private lateinit var logcatCollector: LogcatCollector
+    
     override fun onCreate() {
         super.onCreate()
+        
+        // ВАЖНО: Запускаем коллектор логов ПЕРВЫМ ДЕЛОМ
+        logcatCollector = LogcatCollector.getInstance(this)
+        logcatCollector.startCollecting()
+        
+        android.util.Log.d("App", "Application started with LogcatCollector")
+        
+        // Хук для сохранения при завершении
+        Runtime.getRuntime().addShutdownHook(Thread {
+            logcatCollector.forceSave()
+        })
+    }
+    
+    override fun onTerminate() {
+        super.onTerminate()
+        logcatCollector.stopCollecting()
     }
 }
